e0f9b6e6792a2a43ac66b76f38b5eaeb446f191b
Tam <Tam@users.noreply.github.com>
2017-10-04 10:03:13 +0100

    Update createElement.js

--- createElement.js
+++ createElement.js
@@ -11,28 +11,36 @@
  *     as text).
  * @return {Element} - The created element
  */
-function createElement (tag = "div", attributes = {}, children = []) {
+export default function createElement (tag = "div", attributes = {}, children = []) {
 	const elem = document.createElement(tag);
-
+	
 	for (let [key, value] of Object.entries(attributes)) {
+		if (!value) continue;
+		
 		if (typeof value === typeof (() => {})) {
-			elem.addEventListener(key, value);
+			if (key === "ref") value(elem);
+			else elem.addEventListener(key, value);
 			continue;
 		}
-
+		
+		if (key === "style")
+			value = value.replace(/[\t\r\n]/g, " ").trim();
+		
 		elem.setAttribute(key, value);
 	}
-
+	
 	if (!Array.isArray(children))
 		children = [children];
-
+	
 	children.map(child => {
+		if (!child) return;
+		
 		try {
 			elem.appendChild(child);
 		} catch (_) {
 			elem.appendChild(document.createTextNode(child));
 		}
 	});
-
+	
 	return elem;
 }

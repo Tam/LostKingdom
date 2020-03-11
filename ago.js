9d9e6e22f09abaf63f788856e11ba2a5663b21b1
Tam <Tam@users.noreply.github.com>
2020-03-11 12:02:15 +0000

    Refactor class out of ago

--- ago.js
+++ ago.js
@@ -1,71 +1,64 @@
-export default class Ago {
+const LOCALES = {
+	prefix:    '',
+	suffix:    'ago',
+	separator: ' ',
 
-	// Properties
-	// =========================================================================
+	seconds: 'less than a minute',
+	minute:  'about a minute',
+	minutes: '%d minutes',
+	hour:    'about an hour',
+	hours:   'about %d hours',
+	day:     'a day',
+	days:    '%d days',
+	month:   'about a month',
+	months:  '%d months',
+	year:    'about a year',
+	years:   '%d years',
+};
 
-	static locales = {
-		prefix:    '',
-		suffix:    'ago',
-		separator: ' ',
+/**
+ * Converts the given date to time ago in words
+ *
+ * @param {Date|string|number} timeAgo
+ * @return {string}
+ */
+export default function ago (timeAgo) {
+	if (typeof timeAgo === 'string')
+		timeAgo = new Date(timeAgo);
 
-		seconds: 'less than a minute',
-		minute:  'about a minute',
-		minutes: '%d minutes',
-		hour:    'about an hour',
-		hours:   'about %d hours',
-		day:     'a day',
-		days:    '%d days',
-		month:   'about a month',
-		months:  '%d months',
-		year:    'about a year',
-		years:   '%d years',
+	if (timeAgo instanceof Date)
+		timeAgo = timeAgo.getTime();
+
+	const separator = LOCALES.separator || ' ';
+
+	let seconds = Math.floor((new Date() - parseInt(timeAgo)) / 1000),
+		interval,
+		words = LOCALES.prefix + separator;
+
+	const intervals = {
+		year:   seconds / 31536000,
+		month:  seconds / 2592000,
+		day:    seconds / 86400,
+		hour:   seconds / 3600,
+		minute: seconds / 60
 	};
 
-	// Methods
-	// =========================================================================
+	let distance = LOCALES.seconds;
 
-	/**
-	 * Ago.inWords(myDate);
-	 *
-	 * @param {Date|number} timeAgo
-	 * @return {string}
-	 */
-	static inWords (timeAgo) {
-		if (timeAgo instanceof Date)
-			timeAgo = timeAgo.getTime();
+	for (let key in intervals) {
+		interval = Math.floor(intervals[key]);
 
-		const separator = this.locales.separator || ' ';
-
-		let seconds = Math.floor((new Date() - parseInt(timeAgo)) / 1000),
-			interval,
-			words = this.locales.prefix + separator;
-
-		const intervals = {
-			year:   seconds / 31536000,
-			month:  seconds / 2592000,
-			day:    seconds / 86400,
-			hour:   seconds / 3600,
-			minute: seconds / 60
-		};
-
-		let distance = this.locales.seconds;
-
-		for (let key in intervals) {
-			interval = Math.floor(intervals[key]);
-
-			if (interval > 1) {
-				distance = this.locales[key + 's'];
-				break;
-			} else if (interval === 1) {
-				distance = this.locales[key];
-				break;
-			}
+		if (interval > 1) {
+			distance = LOCALES[key + 's'];
+			break;
+		} else if (interval === 1) {
+			distance = LOCALES[key];
+			break;
 		}
-
-		distance = distance.replace(/%d/i, interval);
-		words += distance + separator + this.locales.suffix;
-
-		return words.trim();
 	}
 
+	distance = distance.replace(/%d/i, interval);
+	words += distance + separator + LOCALES.suffix;
+
+	return words.trim();
 }

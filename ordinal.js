60dcc797e37fbcee49d3d8d32e4549f5ae13f407
Tam <Tam@users.noreply.github.com>
2020-10-22 11:09:01 +0100

    Update ordinal.js

--- ordinal.js
+++ ordinal.js
@@ -5,9 +5,10 @@
  * ordinal(21) => 21st
  * ordinal(22) => 22nd
  * ordinal(23) => 23rd
- *
+ * 
  * @param {number} n
  * @return {string}
+ * @see https://stackoverflow.com/a/44418732/550109
  */
 export default function ordinal (n) {
 	return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');

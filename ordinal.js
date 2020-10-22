277c6e5b84a09a389ee4a48d5b88444dd0800c0f
Tam <Tam@users.noreply.github.com>
2020-10-22 11:08:04 +0100

    Create ordinal.js

---
+++ ordinal.js
@@ -0,0 +1,14 @@
+/**
+ * Will take a given number and append the appropriate ordinal
+ *
+ * ordinal(20) => 20th
+ * ordinal(21) => 21st
+ * ordinal(22) => 22nd
+ * ordinal(23) => 23rd
+ *
+ * @param {number} n
+ * @return {string}
+ */
+export default function ordinal (n) {
+	return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
+}

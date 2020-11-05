f12efba977514775c32d7f5ea550a0923709570e
Tam <Tam@users.noreply.github.com>
2020-11-05 12:27:29 +0000

    Update until.js

--- until.js
+++ until.js
@@ -1,23 +1,25 @@
 /**
  * Get the time from now until the given date in the format of
- * X years, X months, X weeks, X days, X hours, and X minutes
+ * X years, X months, X weeks, and X days
  * hiding and pluralising values accordingly.
  *
  * @param {Date|number|string} date
+ * @param {Date|number|string|null} now
  * @return {string|null}
  */
-export default function until (date) {
+export default function until (date, now = null) {
 	date = new Date(date);
-	const now = new Date();
+	now = now ? new Date(now) : new Date();
 
 	if (date < now)
 		return null;
 
 	let y = date.getFullYear() - now.getFullYear(),
 		m = date.getMonth() - now.getMonth(),
-		d = date.getDate() - now.getDate(),
-		h = date.getHours() - now.getHours(),
-		i = date.getMinutes() - now.getMinutes();
+		d = date.getDate() - now.getDate();
+
+	if (y + m + d === 0)
+		return '1 day';
 
 	const ny = now.getFullYear();
 
@@ -52,10 +54,6 @@ export default function until (date) {
 	const w = Math.floor(d / 7);
 
 	if (w > 0) d %= 7;
-	if (h > 0 && i < 0) {
-		h--;
-		i += 60;
-	}
 
 	const s = v => v === 1 ? '' : 's';
 
@@ -64,7 +62,5 @@ export default function until (date) {
 		m > 0 ? `${m} month${s(m)}` : null,
 		w > 0 ? `${w} week${s(w)}` : null,
 		d > 0 ? `${d} day${s(d)}` : null,
-		h > 0 ? `${h} hour${s(h)}` : null,
-		i > 0 ? `${i} minute${s(i)}` : null,
 	].filter(Boolean).join(', ').replace(/,([^,]*)$/, ', and $1');
 }

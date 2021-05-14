02355904915718ea1aebed942770e3dff39aad09
Tam <Tam@users.noreply.github.com>
2021-05-14 12:01:16 +0100

    Support dates in strings in Ordinal

--- content/ordinal.js
+++ content/ordinal.js
@@ -1,7 +1,3 @@
-/*---
-tags: date,format
-description: Will take a given number and append the appropriate ordinal
----*/
 /**
  * Will take a given number and append the appropriate ordinal
  *
@@ -9,11 +5,14 @@ description: Will take a given number and append the appropriate ordinal
  * ordinal(21) => 21st
  * ordinal(22) => 22nd
  * ordinal(23) => 23rd
+ * ordinal('March 15 2020') => March 15th 2020
  *
- * @param {number} n
+ * @param {number|string} n
  * @return {string}
- * @see https://stackoverflow.com/a/44418732/550109
  */
 export default function ordinal (n) {
+	if (typeof n === 'string')
+		return n.replaceAll(/\b(\d{1,2})\b/gm, (_, b) => ordinal(+b));
+
 	return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
 }

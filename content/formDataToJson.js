cde4473ef8dc0676db3d92c896e8ff6b857b003b
Tam <Tam@users.noreply.github.com>
2021-05-28 15:44:14 +0100

    Create formDataToJson.js

---
+++ content/formDataToJson.js
@@ -0,0 +1,23 @@
+/**
+ * Converts the given FormData to a JSON String
+ *
+ * @param {FormData} formData
+ * @returns {string}
+ */
+export default function formDataToJson (formData) {
+	const object = {};
+
+	formData.forEach((value, key) => {
+		if(!Reflect.has(object, key)){
+			object[key] = value;
+			return;
+		}
+
+		if(!Array.isArray(object[key]))
+			object[key] = [object[key]];
+
+		object[key].push(value);
+	});
+
+	return JSON.stringify(object);
+}

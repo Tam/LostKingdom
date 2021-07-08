8c2eb5197298023a1de1bb039c925cf64f703a42
Tam <Tam@users.noreply.github.com>
2021-07-08 11:02:34 +0100

    Update shuffle.js

--- content/shuffle.js
+++ content/shuffle.js
@@ -1,13 +1,18 @@
 /*---
-tags: array
-description: Randomly shuffles the given array
+tags: array,random,shuffle,mix
+description: Randomly shuffles the given array using Durstenfeld
 ---*/
 /**
- * Randomly shuffles the given array
+ * Randomly shuffles the given array using Durstenfeld
  *
  * @param {array} array
  * @returns {array}
  */
 export default function shuffle (array) {
-	return array.sort(() => Math.random() - 0.5);
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+	
+	return array;
 }

e9af09a10c4958ff64ee4612d5873ac899755f33
Tam <Tam@users.noreply.github.com>
2022-03-14 11:04:11 +0000

    Update kebab.js

--- content/kebab.js
+++ content/kebab.js
@@ -6,6 +6,8 @@ description: Converts the given string to kebab-case
  * Converts the given string to kebab-case
  */
 export default function kebab (str) {
+	if (str === str.toUpperCase()) str = str.toLowerCase();
+	
 	return str.replace(
 		/[\sA-Z\u00C0-\u00D6\u00D8-\u00DE]/g,
 		match => '-' + match.toLowerCase().replace(' ', '')

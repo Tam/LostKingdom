234a19011ce2360191706587b4a7ff3b651f9381
Tam <Tam@users.noreply.github.com>
2023-08-29 11:25:04 +0100

    Create slugify.js

---
+++ content/slugify.js
@@ -0,0 +1,27 @@
+/*---
+tags: string
+description: Converts the given string to slug-i-fied
+---*/
+/**
+ * Slugify a given string
+ *
+ * console.log(slugify("The Quick Brown Fox Jumps Over The Lazy Dog! "))
+ * // "the-quick-brown-fox-jumps-over-the-lazy-dog"
+ *
+ * console.log(slugify("söme stüff with áccènts"))
+ * // "some-stuff-with-accents"
+ *
+ * @param {string} str
+ * @return {string}
+ * @see https://byby.dev/js-slugify-string
+ */
+export default function slugify (str) {
+	return String(str)
+		.normalize('NFKD') // split accented characters into their base characters and diacritical marks
+		.replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+		.trim() // trim leading or trailing whitespace
+		.toLowerCase() // convert to lowercase
+		.replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
+		.replace(/\s+/g, '-') // replace spaces with hyphens
+		.replace(/-+/g, '-') // remove consecutive hyphens
+}
